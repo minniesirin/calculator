e38c893ddeff2237d28704e999d587c506eaf6e2
minnie <sirintharat.p@gmail.com>
2026-06-22 16:31:13 +0700

    create clear and delete button

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function addToDisplay(x){
     display.value += x
     display.focus()
 }
+
+function delDisplay(){
+    let st = display.selectionStart
+    let en = display.selectionEnd
+    let cur = display.value
+
+    let befcur, aftcur
+    aftcur = cur.slice(en)
+
+    if(st === en && st > 0){
+        // delete single char
+        befcur = cur.slice(0, st-1)
+        display.value = befcur + aftcur
+        display.setSelectionRange(st-1, st-1)
+    }
+    else{
+        // delete all selected
+        befcur = cur.slice(0, st)
+        display.value = befcur + aftcur
+        display.setSelectionRange(st, st)
+    }
+
+    display.focus()
+}
+
+function clearDisplay(){
+    display.value = ""
+}
